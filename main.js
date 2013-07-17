98ad5f00c3e0b68d7cfcb7ddbd88a5a3e526297f
jamie holliday <jamie.holliday@ignitiononline.com>
2013-07-17 14:04:46 +0100

    convert to OOP

--- main.js
+++ main.js
@@ -1,83 +1,79 @@
-$(function(){
-	var $convert = $('.convert'),
-		$input = $('.input'),
-		$output = $('.output'),
-		convertIt;
+var Converter = function(options){
+	var inbox = options.input || '.input';
+	var outbox = options.output || '.output';
+	this.$input = $(inbox);
+	this.$output = $(outbox);
+};
 
-	parseText = function($param){
-		var name = $param.attr('name'),
-			styleChange = $param.attr('allowStyleChange'),
-			font = $param.attr('font'),
-			xml = ['<param type="text" '];
-			value = $param.text();
+Converter.prototype.parseText = function($param){
+	var name = $param.attr('name'),
+		styleChange = $param.attr('allowStyleChange'),
+		font = $param.attr('font'),
+		xml = ['<param type="text" ']; 
+		value = $param.text();
 
-			xml.push('name="'+name+'" ');
-			if(styleChange) xml.push('allowStyleChange="'+styleChange+'" ');
-			if(font) xml.push('font="'+font+'" ');
-			xml.push('description="'+name+'" ');
-			xml.push('label="'+name+'" ');
-			xml.push('value="'+value+'" ');
-			xml.push('/>');
+		xml.push('name="'+name+'" ');
+		if(styleChange) xml.push('allowStyleChange="'+styleChange+'" ');
+		if(font) xml.push('font="'+font+'" ');
+		xml.push('description="'+name+'" ');
+		xml.push('label="'+name+'" ');
+		xml.push('value="'+value+'" ');
+		xml.push('/>');
 
-		return xml.join('');
-	};
+	return xml.join('');
+};
 
-	parseRepository = function($param){
-		var name = $param.attr('name'),
-			value = $param.text();
-			xml = ['<param type="repository" '];
+Converter.prototype.parseRepository = function($param){
+	var name = $param.attr('name'),
+		value = $param.text();
+		xml = ['<param type="repository" '];
 
-			xml.push('name="'+name+'" ');
-			xml.push('allowedExt="png|jpg|jpeg|jpe|swf|gif" ');
-			xml.push('description="'+name+'" ');
-			xml.push('label="'+name+'" ');
-			xml.push('required="no" ');
-			xml.push('value="'+value+'" ');
-			xml.push('/>');
+		xml.push('name="'+name+'" ');
+		xml.push('allowedExt="png|jpg|jpeg|jpe|swf|gif" ');
+		xml.push('description="'+name+'" ');
+		xml.push('label="'+name+'" ');
+		xml.push('required="no" ');
+		xml.push('value="'+value+'" ');
+		xml.push('/>');
 
-		return xml.join('');
-	};
+	return xml.join('');
+};
 
-	convertIt = function(){
-		$output.text('');
-		if($input.val()){
-			var val = $input.val(),
+Converter.prototype.convert = function(){
+	this.$output.text('');
+	if(!this.$input.val()) return;
 
-				$val = $(val),
-				xml = [], 
-				params;
+	var val = this.$input.val(),
+		$val = $(val),
+		xml = [], 
+		params, test,
+		self = this;
 
-				//convert the text to XML
-				var test = new DOMParser().parseFromString(val, 'text/xml');
+	//convert the text to XML
+	test = new DOMParser().parseFromString(val, 'text/xml');
 
-			xml.push('<flashtemplate name="XXX - XXX" description="Interaction" tracked="no" width="xxx" height="xxx">');
-			xml.push('\n\n');
-			params = $(test).find('param'); 
-			
-			$(params).each(function(){
-				var $node = $(this),
-					parsed;
+	xml.push('<flashtemplate name="XXX - XXX" description="Interaction" tracked="no" width="xxx" height="xxx">');
+	xml.push('\n\n');
+	params = $(test).find('param'); 
+	
+	$(params).each(function(){
+		var $node = $(this),
+			parsed;
 
-				switch($node.attr('type')){
-					case 'text':
-					parsed = parseText($node);
-					break;
+		switch($node.attr('type')){
+			case 'text':
+			parsed = self.parseText($node);
+			break;
 
-					case 'repository':
-					parsed = parseRepository($node);
-					break;
-				}
-
-				xml.push(parsed);
-				xml.push('\n\n');
-			});
-
-			xml.push('</flashtemplate>');
-			$output.text(xml.join(''));
-			console.log(xml.join(''))
+			case 'repository':
+			parsed = self.parseRepository($node);
+			break;
 		}
-	};
 
-	$convert.on('click', convertIt);
+		xml.push(parsed);
+		xml.push('\n\n');
+	});
 
-});
+	xml.push('</flashtemplate>');
+	this.$output.text(xml.join(''));
+};
